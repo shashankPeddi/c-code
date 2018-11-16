36817b74935ee4bb238a9495ec5fa3dc98feda0c
saishashank <45058716+srishashank@users.noreply.github.com>
2018-11-16 19:31:35 +0530

    strings formates

---
+++ basic.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() 
+{
+
+        
+    printf("C");
+    printf("\n");
+    printf("Language");
+    printf("\n");
+    printf("Welcome To C!!");
+    
+    return 0;
+}
